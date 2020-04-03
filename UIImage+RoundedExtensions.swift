2d406012b19d1f3931822ca5b54b75a2fa82ac4e
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-04-03 14:23:34 -0400

    Updated image rounding.

--- UIImage+RoundedExtensions.swift
+++ UIImage+RoundedExtensions.swift
@@ -9,9 +9,8 @@ extension UIImageView {
     func makeRounded() {
         self.layer.borderWidth = 1
         self.layer.masksToBounds = false
-        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderColor = UIColor.clear.cgColor
         self.layer.cornerRadius = self.frame.height / 2
         self.clipsToBounds = true
     }
 }
-
